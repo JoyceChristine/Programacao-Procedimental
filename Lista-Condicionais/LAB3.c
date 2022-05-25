1d0cd7522252ce7299d9ca0c08fac76f5cd73715
JoyceChristine <70724350+JoyceChristine@users.noreply.github.com>
2022-05-25 20:49:57 -0300

    Create LAB3.c

---
+++ Lista-Condicionais/LAB3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+//Exerc1
+/*int main()
+{
+int num1, num2;
+
+printf("Digite dois numeros inteiros: " );
+scanf("%d%d", &num1, &num2);
+
+if(num1 > num2){
+printf("O Numero %d e maior que numero %d", num1, num2);
+}else{
+if(num1 < num2){
+printf("O Numero %d e maior que numero %d", num2, num1);
+}else{
+printf("O Numero %d e igual numero %d", num2, num1);
+}
+
+}
+
+return 0;
+}*/
+
+//Exerc2
+/*int main()
+{
+int num, a;
+
+printf("Digite um numero: " );
+scanf("%d", &num);
+
+if(num > 1){
+a = sqrt(num);
+printf("Raiz de um numero positivo e %d", a);
+}else{
+if(num < 0){
+printf("Numero invalido");
+}
+}
+
+return 0;
+}*/
+
+//Exerc3
+/*int main()
+{
+float num, a, b;
+
+printf("Digite um numero: " );
+scanf("%f", &num);
+
+if(num > 1){
+a = sqrt(num);
+printf("Raiz do numero digitado e %f", a);
+}else{
+if(num < 0){
+    b = -(num*num);
+    printf("Quadrado do numero e %f", b);
+}
+
+}
+
+return 0;
+}*/
+
+
+//Exerc4
+/*int main()
+{
+float num, a, b;
+
+printf("Digite um numero: " );
+scanf("%f", &num);
+
+if(num > 1){
+a = sqrt(num);
+b = num*num;
+printf("Raiz do numero digitado e %f e o quadrado do numero e %f", a, b);
+}else
+
+return 0;
+}*/
+
+//Exerc5
+/*int main()
+{
+int num, a;
+
+printf("Digite um numero: " );
+scanf("%d", &num);
+
+
+if(a = num%2==0){
+
+printf("Numero e par", a );
+
+}else{
+
+printf("Numero impar");
+
+}
+
+return 0;
+}*/
+
